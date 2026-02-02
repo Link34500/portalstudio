b4540427c0c3887750c195c02bb37fd44daaf325
Link34500 <fredi.guiose@gmail.com>
2026-02-02 01:34:24 -0300

    feat: Integrate Astro sitemap, update robots.txt to allow crawling, and standardize the site URL to include www.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://portalstudio.fr",
+  site: "https://www.portalstudio.fr",
+
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
